4e415f7c40d5d7069da2dfda815999826df7736c
Machahary07 <jeumachahary07@gmail.com>
2024-12-18 15:00:56 +0530

    Minor

--- my-project/routes/_app.tsx
+++ my-project/routes/_app.tsx
@@ -16,7 +16,7 @@ export default function App() {  // Add parentheses here
       </head>
       <body>
         <section>
-          {/* <Navbar /> */}
+          <Navbar />
           <Hero />
           {/* <Work /> */}
           <Work2/>
